dba36edb2731882acedb22d3b6cbcd38798ccf2d
Karine St-Onge <kstonge@ferlab.bio>
2023-04-19 06:31:56 -0400

    feat: CQDG-228 Setup Cypress tests

---
+++ cypress.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'cypress';
+
+const date = new Date();
+const joinWithPadding = (l: number[]) => l.reduce((xs, x) => xs + `${x}`.padStart(2, '0'), '');
+const strDate = joinWithPadding([
+  date.getFullYear(),
+  date.getMonth() + 1,
+  date.getDate()
+]);
+const strTime = joinWithPadding([
+  date.getHours(),
+  date.getMinutes()
+]);
+
+export default defineConfig({
+  chromeWebSecurity: true,
+  video: false,
+  videoUploadOnPasses: false,
+  screenshotOnRunFailure: true,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config);
+    },
+    baseUrl: 'https://portalv2.qa.cqdg.ferlab.bio',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    slowTestThreshold: 60000,
+    experimentalSessionAndOrigin: true
+  },
+  retries: {
+    "runMode": 2,
+    "openMode": 0
+  },
+  reporter: 'junit',
+  reporterOptions: {
+     "mochaFile": 'cypress/results/'+strDate+'_'+strTime+'-[hash].xml',
+     rootSuiteTitle: 'Tests Cypress'
+  }
+});
---
+++ cypress/e2e/Colonnes/TableauVariants.cy.ts
@@ -0,0 +1,45 @@
+/// <reference types="Cypress" />
+import '../../support/commands';
+
+beforeEach(() => {
+  cy.login();
+});
+
+describe('Tableau des variants', () => {
+  describe('Personnaliser les colonnes', () => {
+
+    beforeEach(() => {
+      cy.visitVariantsPage();
+    });
+
+    it('Valider l\'affichage (par défaut/optionnel) et l\'ordre des colonnes', () => {
+      cy.get('thead[class="ant-table-thead"]')
+        .find('th[class*="ant-table-cell"]').eq(1)
+        .contains('Variant').should('exist');
+      
+      cy.get('thead[class="ant-table-thead"]')
+        .find('th[class*="ant-table-cell"]').eq(2)
+        .should('have.class', 'ant-table-column-has-sorters')
+        .contains('Type').should('exist');
+    });
+
+    it('Masquer/Afficher une colonne affichée', () => {
+      cy.get('thead[class="ant-table-thead"]')
+        .contains('Type').should('exist');
+
+      cy.get('div[class="ant-popover-inner"]')
+        .find('div[class="ant-space-item"]').contains('Type')
+        .find('[type="checkbox"]').uncheck({force: true});
+
+      cy.get('thead[class="ant-table-thead"]')
+        .contains('Type').should('not.exist');
+
+      cy.get('div[class="ant-popover-inner"]')
+        .find('div[class="ant-space-item"]').contains('Type')
+        .find('[type="checkbox"]').check({force: true});
+
+      cy.get('thead[class="ant-table-thead"]')
+        .contains('Type').should('exist');
+    });
+  });
+});
