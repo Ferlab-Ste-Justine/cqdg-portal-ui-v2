74ea5d6296137d1935ed7270035a906c92743466
Adrian <adipaul1981@yahoo.ca>
2023-11-08 13:37:01 -0500

    Revert "fix: CQDG-455 remove more less from Data access in studies page"
    This reverts commit d244650a096bf9797958ea4bc91f42ac2b6dce26.

--- src/views/StudyEntity/utils/getDataAccessDescriptions.tsx
+++ src/views/StudyEntity/utils/getDataAccessDescriptions.tsx
@@ -1,5 +1,6 @@
 import intl from 'react-intl-universal';
 import ExternalLink from '@ferlab/ui/core/components/ExternalLink';
+import ExpandableCell from '@ferlab/ui/core/components/tables/ExpandableCell';
 import { IEntityDescriptionsItem } from '@ferlab/ui/core/pages/EntityPage';
 import { IStudyEntity } from 'graphql/studies/models';
 import capitalize from 'lodash/capitalize';
@@ -9,40 +10,36 @@ import { TABLE_EMPTY_PLACE_HOLDER } from 'common/constants';
 
 import styles from '../index.module.scss';
 
-type INonExpandableCellProps = {
-  dataSource: string[];
-  className?: string;
-};
-const NonExpandableCell = ({ dataSource }: INonExpandableCellProps) => (
-  <>
-    {dataSource.map((d, index) => {
-      const duoTerm = extractDuoTitleAndCode(d);
-      return duoTerm ? (
-        <div key={index}>
-          {capitalize(duoTerm.title)} (DUO:{' '}
-          <ExternalLink
-            href={`http://purl.obolibrary.org/obo/DUO_${duoTerm.code}`}
-            className={styles.link}
-          >
-            {duoTerm.code}
-          </ExternalLink>
-          )
-        </div>
-      ) : (
-        TABLE_EMPTY_PLACE_HOLDER
-      );
-    })}
-  </>
-);
-
 const getDataAccessDescriptions = (study?: IStudyEntity): IEntityDescriptionsItem[] => [
   {
     label: intl.get('entities.study.access_limitation'),
     value: study?.data_access_codes?.access_limitations?.length ? (
-      <NonExpandableCell
+      <ExpandableCell
+        nOfElementsWhenCollapsed={1}
+        dataSource={study.data_access_codes.access_limitations}
+        dictionnary={{
+          'see.less': intl.get('global.seeLess'),
+          'see.more': intl.get('global.seeMore'),
+        }}
         className={styles.forceLinkUnderline}
-        dataSource={study?.data_access_codes.access_limitations}
-      ></NonExpandableCell>
+        renderItem={(access_limitation, index) => {
+          const limitation = extractDuoTitleAndCode(access_limitation);
+          return limitation ? (
+            <div key={index}>
+              {capitalize(limitation.title)} (DUO:{' '}
+              <ExternalLink
+                href={`http://purl.obolibrary.org/obo/DUO_${limitation.code}`}
+                className={styles.link}
+              >
+                {limitation.code}
+              </ExternalLink>
+              )
+            </div>
+          ) : (
+            TABLE_EMPTY_PLACE_HOLDER
+          );
+        }}
+      />
     ) : (
       TABLE_EMPTY_PLACE_HOLDER
     ),
@@ -50,10 +47,32 @@ const getDataAccessDescriptions = (study?: IStudyEntity): IEntityDescriptionsIte
   {
     label: intl.get('entities.study.access_requirement'),
     value: study?.data_access_codes?.access_requirements?.length ? (
-      <NonExpandableCell
+      <ExpandableCell
+        nOfElementsWhenCollapsed={1}
+        dataSource={study.data_access_codes.access_requirements}
+        dictionnary={{
+          'see.less': intl.get('global.seeLess'),
+          'see.more': intl.get('global.seeMore'),
+        }}
         className={styles.forceLinkUnderline}
-        dataSource={study?.data_access_codes.access_requirements}
-      ></NonExpandableCell>
+        renderItem={(access_requirement, index) => {
+          const requirement = extractDuoTitleAndCode(access_requirement);
+          return requirement ? (
+            <div key={index}>
+              {capitalize(requirement.title)} (DUO:{' '}
+              <ExternalLink
+                href={`http://purl.obolibrary.org/obo/DUO_${requirement.code}`}
+                className={styles.link}
+              >
+                {requirement.code}
+              </ExternalLink>
+              )
+            </div>
+          ) : (
+            TABLE_EMPTY_PLACE_HOLDER
+          );
+        }}
+      />
     ) : (
       TABLE_EMPTY_PLACE_HOLDER
     ),
