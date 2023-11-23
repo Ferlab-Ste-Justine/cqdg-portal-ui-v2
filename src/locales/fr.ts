5ea1f17572e8a55c5c2da2d5b3b767b7163a8d67
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2023-11-23 14:36:13 -0500

    feat: CQDG-490 refont age categories 2

--- src/locales/fr.ts
+++ src/locales/fr.ts
@@ -98,17 +98,6 @@ const fr = {
       hpo_term_tooltip: '# de participants avec le terme HPO exact',
       observed: 'Observé',
       not_observed: 'Non observé',
-      ageCategories: [
-        { key: 'A-antenatal', label: 'Antenatal', tooltip: '' },
-        { key: 'B-congenital', label: 'Congenital', tooltip: '' },
-        { key: 'C-neonatal', label: 'Neonatal', tooltip: '< 28 days' },
-        { key: 'D-infantile', label: 'Infantile', tooltip: '>= 28 days and < 1 year' },
-        { key: 'E-childhood', label: 'Childhood', tooltip: '>= 1 year and < 5 years)' },
-        { key: 'F-juvenile', label: 'Juvenile', tooltip: '>= 5 years and < 16 years' },
-        { key: 'G-youngadult', label: 'Young Adult', tooltip: '>= 16 years and < 40 years' },
-        { key: 'H-middleage', label: 'Middle Age', tooltip: '>= 40 years and < 60 years' },
-        { key: 'I-senior', label: 'Senior', tooltip: '>= 60 years' },
-      ],
     },
     file: {
       file: 'Fichier',
