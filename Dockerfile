d8b86c0216b256fad9233d954ce0033eb47551d0
ihannache <109966217+ihannache@users.noreply.github.com>
2023-11-29 08:35:39 -0500

    feat: INFRA-403 final version of portal optimization (#204)
    * feat: INFRA-403 final version of portal optimization
    
    * feat: INFRA-403 update package-lock
    
    * feat: INFRA-403 prettier
    
    ---------
    
    Co-authored-by: Adrian <adipaul1981@yahoo.ca>

--- Dockerfile
+++ Dockerfile
@@ -17,3 +17,12 @@ FROM nginx:1.24
 COPY --from=build-stage /app/build/ /usr/share/nginx/html
 
 COPY static.conf /etc/nginx/conf.d/default.conf
+
+# Copy the entrypoint script into the image
+COPY entrypoint.sh /entrypoint.sh
+
+# Make the script executable
+RUN chmod +x /entrypoint.sh
+
+# Set the entrypoint
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Generate a unique filename for config.js based on the current timestamp
+UNIQUE_CONFIG_FILENAME="config-$(date +%s).js"
+
+# Generate config.js with a unique filename
+cat <<EOF > "/usr/share/nginx/html/$UNIQUE_CONFIG_FILENAME"
+window._env_ = {
+  REACT_APP_ARRANGER_API_URL: "$REACT_APP_ARRANGER_API_URL",
+  REACT_APP_ARRANGER_PROJECT_ID: "$REACT_APP_ARRANGER_PROJECT_ID",
+  REACT_APP_KC_AUTH_SERVER_URL: "$REACT_APP_KC_AUTH_SERVER_URL",
+  REACT_APP_KC_CLIENT_ID: "$REACT_APP_KC_CLIENT_ID",
+  REACT_APP_KC_REALM: "$REACT_APP_KC_REALM",
+  SASS_PATH: "$SASS_PATH",
+  REACT_APP_USERS_API_URL: "$REACT_APP_USERS_API_URL",
+  REACT_APP_CQDG_WEB_APP: "$REACT_APP_CQDG_WEB_APP",
+  REACT_APP_CQDG_WEB_SITE: "$REACT_APP_CQDG_WEB_SITE",
+  REACT_APP_CQDG_DOCUMENTATION: "$REACT_APP_CQDG_DOCUMENTATION",
+  REACT_APP_USER_SNAP_API_KEY: "$REACT_APP_USER_SNAP_API_KEY",
+  REACT_APP_REPORTS_API_URL: "$REACT_APP_REPORTS_API_URL"
+};
+EOF
+
+# Dynamically update the index.html to reference the new unique config.js filename
+sed -i "s|/config.js|$UNIQUE_CONFIG_FILENAME|" /usr/share/nginx/html/index.html
+
+# Start Nginx
+nginx -g "daemon off;"
