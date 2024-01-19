e120d05bb0ad55482c0393f794cd87792940615f
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2024-01-19 14:50:01 -0500

    feat: CQDG-445 remove summary tab changes

--- src/views/DataExploration/components/PageContent/tabs/Summary/index.tsx
+++ src/views/DataExploration/components/PageContent/tabs/Summary/index.tsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import ResizableGridLayout, {
   TSerializedResizableGridLayoutConfig,
 } from '@ferlab/ui/core/layout/ResizableGridLayout';
+import { Space } from 'antd';
 
 import { useUser } from 'store/user';
 import { updateUserConfig } from 'store/user/thunks';
@@ -9,23 +10,27 @@ import { getResizableGridDictionary } from 'utils/translation';
 
 import getSummaryLayout, { UID } from './getSummaryLayout';
 
+import styles from './index.module.scss';
+
 const SummaryTab = () => {
   const dispatch = useDispatch();
   const { userInfo } = useUser();
 
   return (
-    <ResizableGridLayout
-      uid={UID}
-      defaultLayouts={getSummaryLayout()}
-      dictionary={getResizableGridDictionary()}
-      layouts={userInfo?.config.data_exploration?.summary?.layouts}
-      onReset={(layouts: TSerializedResizableGridLayoutConfig[]) => {
-        dispatch(updateUserConfig({ data_exploration: { summary: { layouts } } }));
-      }}
-      onConfigUpdate={(layouts: TSerializedResizableGridLayoutConfig[]) => {
-        dispatch(updateUserConfig({ data_exploration: { summary: { layouts } } }));
-      }}
-    />
+    <Space className={styles.wrapper} direction="vertical">
+      <ResizableGridLayout
+        uid={UID}
+        defaultLayouts={getSummaryLayout()}
+        dictionary={getResizableGridDictionary()}
+        layouts={userInfo?.config.data_exploration?.summary?.layouts}
+        onReset={(layouts: TSerializedResizableGridLayoutConfig[]) => {
+          dispatch(updateUserConfig({ data_exploration: { summary: { layouts } } }));
+        }}
+        onConfigUpdate={(layouts: TSerializedResizableGridLayoutConfig[]) => {
+          dispatch(updateUserConfig({ data_exploration: { summary: { layouts } } }));
+        }}
+      />
+    </Space>
   );
 };
 
