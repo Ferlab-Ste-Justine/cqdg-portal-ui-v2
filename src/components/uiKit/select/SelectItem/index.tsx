3127e58c5c7e87ec46522dfe9464cbbfa5a9a345
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2024-02-27 16:22:32 -0500

    feat: CQDG-621 add tooltip or not

--- src/components/uiKit/select/SelectItem/index.tsx
+++ src/components/uiKit/select/SelectItem/index.tsx
@@ -9,18 +9,36 @@ interface ISelectItemProps {
   icon?: ReactNode;
   title: ReactNode;
   caption?: ReactNode;
+  isTooltip?: boolean;
+  isEllipsis?: boolean;
 }
 
-const SelectItem = ({ icon, title, caption }: ISelectItemProps) => (
-  <Tooltip title={caption} color="white">
-    <Space size={10} align="start">
-      {icon ? <div className={styles.iconWrapper}>{icon}</div> : undefined}
-      <Space direction="vertical" size={0}>
-        {title}
-        <Text type="secondary">{caption}</Text>
-      </Space>
+const SelectItem = ({ icon, title, caption, isEllipsis }: ISelectItemProps) => (
+  <Space size={10} align="start">
+    {icon && <div className={styles.iconWrapper}>{icon}</div>}
+    <Space direction="vertical" size={0}>
+      {title}
+      <Text
+        type="secondary"
+        ellipsis={isEllipsis}
+        className={isEllipsis ? styles.textEllipsis : ''}
+      >
+        {caption}
+      </Text>
     </Space>
-  </Tooltip>
+  </Space>
 );
 
-export default SelectItem;
+export const SelectItemTooltipWrapper = ({ icon, title, caption, isTooltip }: ISelectItemProps) =>
+  isTooltip ? (
+    <Tooltip title={caption} placement="bottom">
+      {/** Tooltip bug, needs empty div here */}
+      <div>
+        <SelectItem icon={icon} title={title} caption={caption} isEllipsis />
+      </div>
+    </Tooltip>
+  ) : (
+    <SelectItem icon={icon} title={title} caption={caption} />
+  );
+
+export default SelectItemTooltipWrapper;
