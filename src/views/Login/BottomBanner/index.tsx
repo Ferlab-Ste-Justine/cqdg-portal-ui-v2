355f15594336974010db38db667a0fbe247e5ffe
Alek Perron <alek.perron.hsj@ssss.gouv.qc.ca>
2024-04-17 15:12:02 -0400

    feat: CQDG-696 use ferlab components in landing page

--- src/views/Login/BottomBanner/index.tsx
+++ src/views/Login/BottomBanner/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import intl from 'react-intl-universal';
 import CloudDatabaseIcon from '@ferlab/ui/core/components/Icons/FuturoSpot/CloudDatabaseSpotIcon';
 import InformationIcon from '@ferlab/ui/core/components/Icons/FuturoSpot/InformationSpotIcon';
-
-import BannerItem from './BannerItem';
+import BannerItem from '@ferlab/ui/core/pages/LandingPage/BannerItem';
 
 import styles from './index.module.scss';
 
@@ -12,17 +11,29 @@ const BottomBanner = () => (
     <div className={styles.content}>
       <BannerItem
         IconComponent={InformationIcon}
-        title={intl.get('screen.loginPage.documentation.title')}
-        description={intl.get('screen.loginPage.documentation.description')}
-        buttonText={intl.get('screen.loginPage.documentation.button')}
-        buttonUrl="https://docs.cqdg.ca/"
+        dictionary={{
+          title: intl.get('screen.loginPage.documentation.title'),
+          description: intl.get('screen.loginPage.documentation.description'),
+          button: intl.get('screen.loginPage.documentation.button'),
+        }}
+        buttonProps={{
+          ghost: true,
+          target: '_blank',
+          href: 'https://docs.cqdg.ca/',
+        }}
       />
       <BannerItem
         IconComponent={CloudDatabaseIcon}
-        title={intl.get('screen.loginPage.hosting.title')}
-        description={intl.get('screen.loginPage.hosting.description')}
-        buttonText={intl.get('screen.loginPage.hosting.button')}
-        buttonUrl="https://docs.cqdg.ca/docs/comment-soumettre-vos-donn%C3%A9es"
+        dictionary={{
+          title: intl.get('screen.loginPage.hosting.title'),
+          description: intl.get('screen.loginPage.hosting.description'),
+          button: intl.get('screen.loginPage.hosting.button'),
+        }}
+        buttonProps={{
+          ghost: true,
+          target: '_blank',
+          href: 'https://docs.cqdg.ca/docs/comment-soumettre-vos-donn%C3%A9es',
+        }}
       />
     </div>
   </div>
--- src/views/Login/Studies/index.tsx
+++ src/views/Login/Studies/index.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
+import intl from 'react-intl-universal';
+import Studies from '@ferlab/ui/core/pages/LandingPage/Studies';
 
-import Carousel from './Carousel';
-import Summary from './Summary';
+import CartageneLogo from '../../../components/assets/cartagene.png';
+import { useGlobals } from '../../../store/global';
 
 import styles from './index.module.scss';
 
-const Studies = () => (
-  <div className={styles.container}>
-    <div className={styles.layout}>
-      <Summary />
-      <Carousel />
+const studies = [
+  { code: 'cartagene', logo: CartageneLogo },
+  { code: 'dee' },
+  { code: 'bacq' },
+  { code: 'pragmatiq' },
+  { code: 'neurodev' },
+];
+
+const formatStudies = () =>
+  studies.map((study) => ({
+    code: study.code,
+    title: study.logo ? (
+      <img src={study.logo} alt="Study Logo" className={styles.logo} />
+    ) : (
+      intl.get(`screen.loginPage.studies.${study.code}.title`)
+    ),
+    subtitle: intl.get(`screen.loginPage.studies.${study.code}.subtitle`),
+    description: intl.getHTML(`screen.loginPage.studies.${study.code}.description`),
+  }));
+
+const StudiesSection = () => {
+  const { stats } = useGlobals();
+  const { studies = 0 } = stats || {};
+  const formattedStudies = formatStudies();
+
+  return (
+    <div className={styles.container}>
+      <Studies
+        studiesCount={studies}
+        studies={formattedStudies}
+        dictionary={{
+          title: intl.get('screen.loginPage.studies.title'),
+          summary: intl.get('screen.loginPage.studies.summary'),
+        }}
+      />
     </div>
-  </div>
-);
-export default Studies;
+  );
+};
+export default StudiesSection;
