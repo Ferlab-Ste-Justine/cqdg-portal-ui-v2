315212a10365b418e74e098d5e8fc2919ef7c7d5
Issam <ihannache@ferlab.bio>
2024-07-17 15:29:33 -0400

    feat: INFRA-403 Switch cypress portal url to internal service

--- cypress.config.ts
+++ cypress.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.ts')(on, config);
     },
-    baseUrl: 'https://portail.qa.juno.cqdg.ferlab.bio/',
+    baseUrl: 'http://portal-ui:3000/',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     slowTestThreshold: 60000,
     experimentalSessionAndOrigin: true,
