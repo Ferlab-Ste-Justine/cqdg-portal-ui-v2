95fe4e0a78abc25b7bfb79740c2003d08ccc685e
Karine St-Onge <kstonge@ferlab.bio>
2024-08-28 14:19:08 -0400

    feat: CQDG-843 Adjust Cypress tests

---
+++ cypress/e2e/Recherche/PageStudies.cy.ts
@@ -0,0 +1,45 @@
+/// <reference types="cypress"/>
+import '../../support/commands';
+
+beforeEach(() => {
+  cy.login();
+  cy.visitStudiesPage();
+});
+
+describe('Page des études - Rechercher des études', () => {
+  it('Par study code', () => {
+    cy.typeAndIntercept('[class*="PageContent_search"]', 'study1', 'POST', '**/graphql', 6);
+    cy.validateTableResultsCount(/1 Result/);
+    cy.validateTableFirstRow('STUDY1', 0);
+
+    cy.get('button[class*="Header_clearFilterLink"]').should('contain', 'Clear filters').clickAndWait({force: true});
+    cy.validateTableResultsCount(/\d{1} Results/);
+  });
+
+  it('Par study name', () => {
+    cy.typeAndIntercept('[class*="PageContent_search"]', 'congenital', 'POST', '**/graphql', 10);
+    cy.validateTableResultsCount(/1 Result/);
+    cy.validateTableFirstRow('STUDY1', 0);
+
+    cy.get('button[class*="Header_clearFilterLink"]').should('contain', 'Clear filters').clickAndWait({force: true});
+    cy.validateTableResultsCount(/\d{1} Results/);
+  });
+
+  it('Par domaine', () => {
+    cy.typeAndIntercept('[class*="PageContent_search"]', 'diseases', 'POST', '**/graphql', 8);
+    cy.validateTableResultsCount(/1 Result/);
+    cy.validateTableFirstRow('STUDY1', 0);
+
+    cy.get('button[class*="Header_clearFilterLink"]').should('contain', 'Clear filters').clickAndWait({force: true});
+    cy.validateTableResultsCount(/\d{1} Results/);
+  });
+
+  it('Par mot-clé', () => {
+    cy.typeAndIntercept('[class*="PageContent_search"]', 'FAMILY', 'POST', '**/graphql', 6);
+    cy.validateTableResultsCount(/2 Results/);
+    cy.validateTableFirstRow('STUDY2', 0);
+
+    cy.get('button[class*="Header_clearFilterLink"]').should('contain', 'Clear filters').clickAndWait({force: true});
+    cy.validateTableResultsCount(/\d{1} Results/);
+  });
+});
