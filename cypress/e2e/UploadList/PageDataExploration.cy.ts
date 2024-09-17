7a2d294848a1a1c224a90c6f81247bf797900af9
Karine St-Onge <kstonge@ferlab.bio>
2024-09-17 13:14:13 -0400

    feat: CQDG-869 Adjust Cypress tests related to CQDG-754

--- cypress/e2e/UploadList/PageDataExploration.cy.ts
+++ cypress/e2e/UploadList/PageDataExploration.cy.ts
@@ -13,7 +13,7 @@ describe('Page Data Exploration (Participants) - Téléverser une liste d\'ident
     cy.visitDataExploration('participants');
     cy.get('[data-cy="SidebarMenuItem_Participant"]').clickAndWait({force: true});
     cy.get('button[class*="UploadIdsButton"]').clickAndWait({force: true});
-    cy.get('[class*="UploadModal"] textarea').type('pt0000010,s03510 unknown');
+    cy.get('[class*="UploadModal"] textarea').type('pt0000010,hsj-1005-389 unknown');
   });
 
   it('Vérifier les informations affichées - Popover', () => {
@@ -43,7 +43,7 @@ describe('Page Data Exploration (Participants) - Téléverser une liste d\'ident
     cy.get('body').contains('Use the search tools & facets on the left to build a query').should('exist');
   });
 
-  it('Valider les fonctionnalités de la modal - Section Résumé masquable [CQDG-754]', () => {
+  it('Valider les fonctionnalités de la modal - Section Résumé masquable', () => {
     cy.get('[class*="UploadModal"] [class="ant-collapse-header-text"]').contains('Summary Table (2 matched, 1 unmatched)').should('exist');
 
     cy.get('[class*="UploadModal"] span[class*="ant-collapse-arrow"]').clickAndWait({force: true});
@@ -53,7 +53,7 @@ describe('Page Data Exploration (Participants) - Téléverser une liste d\'ident
     cy.get('[class*="UploadModal"] div[class*="ant-collapse-content-inactive ant-collapse-content-hidden"]').should('exist');
   });
 
-  it('Vérifier les informations affichées - Section Résumé (onglet Reconnus) [CQDG-754,CQDG-762]', () => {
+  it('Vérifier les informations affichées - Section Résumé (onglet Reconnus) [CQDG-762]', () => {
     cy.get('[class*="UploadModal"] span[class*="ant-collapse-arrow"]').clickAndWait({force: true});
 
     cy.get('[class*="UploadModal_tablesMessages"]').contains('3 submitted identifiers mapped to 1 unique system identifiers').should('exist');
@@ -65,12 +65,12 @@ describe('Page Data Exploration (Participants) - Téléverser une liste d\'ident
     cy.get('[id*="panel-matched"] [data-row-key="PT0000010:0"] td').eq(0).contains('pt0000010').should('exist');
     cy.get('[id*="panel-matched"] [data-row-key="PT0000010:0"] td').eq(1).contains('PT0000010').should('exist');
     cy.get('[id*="panel-matched"] [data-row-key="PT0000010:0"] td').eq(2).contains('T-DEE').should('exist');
-    cy.get('[id*="panel-matched"] [data-row-key="PT0000010:1"] td').eq(0).contains('s03510').should('exist');
+    cy.get('[id*="panel-matched"] [data-row-key="PT0000010:1"] td').eq(0).contains('hsj-1005-389').should('exist');
     cy.get('[id*="panel-matched"] [data-row-key="PT0000010:1"] td').eq(1).contains('PT0000010').should('exist');
     cy.get('[id*="panel-matched"] [data-row-key="PT0000010:1"] td').eq(2).contains('T-DEE').should('exist');
   });
 
-  it('Vérifier les informations affichées - Section Résumé (onglet Inconnus) [CQDG-754,CQDG-762]', () => {
+  it('Vérifier les informations affichées - Section Résumé (onglet Inconnus) [CQDG-762]', () => {
     cy.get('[class*="UploadModal"] span[class*="ant-collapse-arrow"]').clickAndWait({force: true});
     cy.get('[data-node-key="unmatched"]').clickAndWait({force: true});
 
