d4ec81d54835d2c2d8566a43ea8352eb11d6c0df
Karine St-Onge <kstonge@ferlab.bio>
2024-09-23 14:45:01 -0400

    feat: CQDG-835 Adjust Cypress tests

--- cypress/e2e/ManifestButton/PageStudyDataset.cy.ts
+++ cypress/e2e/ManifestButton/PageStudyDataset.cy.ts
@@ -35,7 +35,7 @@ describe('Page d\'une étude (Dataset) - Bouton Manifest', () => {
   it('Vérifier les informations affichées - Modal', () => {
     cy.get('[class="ant-modal-title"]').contains('File manifest').should('exist');
     cy.get('[class="ant-modal-body"]').contains('Download a manifest of this study’s files which can be used with CQDG\'s bulk download tool. This manifest also includes additional information, including the participants and samples associated with these files.').should('exist');
-    cy.get('[class="ant-modal-body"]').contains('Include data files of the same type for the participants\' related family members for this selection.').should('exist');
+    cy.get('[class="ant-modal-body"]').contains('Include data files of the same type for the participants\' related family members for this selection.').should('not.exist');
     cy.get('[class*="DownloadFileManifestModal_table"] thead th').eq(0).contains('Data Type').should('exist');
     cy.get('[class*="DownloadFileManifestModal_table"] thead th').eq(1).contains('Participants').should('exist');
     cy.get('[class*="DownloadFileManifestModal_table"] thead th').eq(2).contains('Files').should('exist');
@@ -57,8 +57,10 @@ describe('Page d\'une étude (Dataset) - Bouton Manifest', () => {
     cy.get('[class*="DownloadFileManifestModal_table"] [data-row-key="Supplement"] td').eq(2).contains(/^3$/).should('exist');
     cy.get('[class*="DownloadFileManifestModal_table"] [data-row-key="Supplement"] td').eq(3).contains(/^0 B$/).should('exist');
 
-    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-default"]').contains('Cancel').should('exist');
-    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"]').contains('Download').should('exist');
+    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"]').eq(0).find('[class*="anticon-copy"]').should('exist');
+    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"]').eq(0).contains('Copy manifest ID').should('exist');
+    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"]').eq(1).find('[class*="anticon-download"]').should('exist');
+    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"]').eq(1).contains('Download').should('exist');
   });
 
   it('Valider les fonctionnalités - Bouton Cancel', () => {
@@ -69,8 +71,26 @@ describe('Page d\'une étude (Dataset) - Bouton Manifest', () => {
     });
   });
 
+  it('Valider les fonctionnalités - Bouton Copy manifest ID', () => {
+    cy.clickAndIntercept('[class="ant-modal-footer"] button[class*="ant-btn-primary"]', 'POST', '**/sets', 1, 0);
+    cy.get('[class="ant-message"]').contains('ID copied to clipboard').should('exist');
+    cy.get('[class="ant-message"]').contains('error').should('not.exist');
+  });
+
+  it('Vérifier les informations affichées - Tooltip du bouton Copy manifest ID', () => {
+    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"] [class*="anticon-copy"]').trigger('mouseover', {eventConstructor: 'MouseEvent', force: true});
+    cy.get('div[class="ant-tooltip-inner"]').contains('Copy the manifest ID for use in ').should('exist');
+    cy.get('div[class="ant-tooltip-inner"]').contains('Ferload').should('exist');
+    cy.get('div[class="ant-tooltip-inner"] [class="anticon"]').should('exist');
+  });
+
+  it('Valider les liens disponibles - Tooltip du bouton Copy manifest ID', () => {
+    cy.get('[class="ant-modal-footer"] button[class*="ant-btn-primary"] [class*="anticon-copy"]').trigger('mouseover', {eventConstructor: 'MouseEvent', force: true});
+    cy.get('div[class="ant-tooltip-inner"] [class*="DownloadFileManifestModal_externalLinkFerload"]').should('have.attr', 'href', 'https://docs.cqdg.ca/docs/comment-utiliser-le-client-ferload?ljs=en-CA');
+  });
+
   it('Valider les fonctionnalités - Bouton Download', () => {
-    cy.clickAndIntercept('[class="ant-modal-footer"] button[class*="ant-btn-primary"]', 'POST', '**/file-manifest', 1);
+    cy.clickAndIntercept('[class="ant-modal-footer"] button[class*="ant-btn-primary"]', 'POST', '**/file-manifest', 1, 1);
     cy.get('[class*="DownloadFileManifestModal_modal"]').should('have.css', 'display', 'none');
     cy.waitUntilFile(oneMinute);
     cy.validateFileName('*.tsv');
@@ -80,33 +100,12 @@ describe('Page d\'une étude (Dataset) - Bouton Manifest', () => {
 describe('Page d\'une étude (Dataset) - Télécharger le manifest', () => {
   beforeEach(() => {
     cy.get('[class*="EntityDataset_container"] [data-cy="FileManifest_Button"]').eq(1).click({force: true});
-    cy.clickAndIntercept('[class="ant-modal-footer"] button[class*="ant-btn-primary"]', 'POST', '**/file-manifest', 1);
+    cy.clickAndIntercept('[class="ant-modal-footer"] button[class*="ant-btn-primary"]', 'POST', '**/file-manifest', 1, 1);
     cy.waitUntilFile(oneMinute);
   });
 
   it('Valider le nom du fichier', () => {
     cy.validateFileName('cqdg_data2_manifest_'+`${strDate.slice(0, 4)}${strDate.slice(4, 6)}${strDate.slice(6, 8)}`+'.tsv');
-  });
-
-  it('Valider les en-têtes du fichier', () => {
-    cy.validateFileHeaders('DownloadManifestStudy.json');
-  });
-
-  it('Valider le contenu du fichier', () => {
-    cy.validateFileContent('DownloadManifestStudy.json');
-  });
-});
-
-describe('Page d\'une étude (Dataset) - Télécharger le manifest (checkbox)', () => {
-  beforeEach(() => {
-    cy.get('[class*="EntityDataset_container"] [data-cy="FileManifest_Button"]').eq(1).click({force: true});
-    cy.get('[class="ant-modal-body"] input[type="checkbox"]').check({force: true});
-    cy.clickAndIntercept('[class="ant-modal-footer"] button[class*="ant-btn-primary"]', 'POST', '**/file-manifest', 1);
-    cy.waitUntilFile(oneMinute);
-  });
-
-  it('Valider le nom du fichier [CQDG-859]', () => {
-    cy.validateFileName('cqdg_data2_familyManifest_'+`${strDate.slice(0, 4)}${strDate.slice(4, 6)}${strDate.slice(6, 8)}`+'.tsv');
   });
 
   it('Valider les en-têtes du fichier', () => {
