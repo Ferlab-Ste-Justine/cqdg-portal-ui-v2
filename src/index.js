4261b0867d9a53e9ab63f291ae811d7e505adfa1
Francis Lavoie <lav.francis+gh@gmail.com>
2024-11-19 12:04:03 -0500

    feat: CQDG-832 add sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import EnvironmentVariables from 'helpers/EnvVariables';
 
 import { initUserSnap } from 'services/initUsersnap';
+import LocalStorageIntegrations from 'services/sentry/localStorageIntegrations';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,6 +16,22 @@ import 'style/themes/cqdg/main.css';
 import './index.css';
 
 initUserSnap();
+
+const reactAppWebRoot = EnvironmentVariables.configFor('CQDG_WEB_APP');
+const SentryDSN = EnvironmentVariables.configFor('SENTRY_API');
+
+Sentry.init({
+  dsn: SentryDSN,
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', reactAppWebRoot],
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    new LocalStorageIntegrations('LocalStorage'),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container);
