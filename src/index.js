d0a4f732779e37e759bf8d722b0b6db114074e61
Francis Lavoie <lav.francis+gh@gmail.com>
2024-11-20 11:46:44 -0500

    feat: CQDG-832 add sentry use updated integration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { localStorageIntegration } from '@ferlab/ui/core/utils/sentry/localStorageIntegration';
 import * as Sentry from '@sentry/react';
 import EnvironmentVariables from 'helpers/EnvVariables';
 
 import { initUserSnap } from 'services/initUsersnap';
-import LocalStorageIntegrations from 'services/sentry/localStorageIntegrations';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -22,12 +22,13 @@ const SentryDSN = EnvironmentVariables.configFor('SENTRY_API');
 
 Sentry.init({
   dsn: SentryDSN,
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', reactAppWebRoot],
   integrations: [
-    Sentry.browserTracingIntegration(),
+    Sentry.browserTracingIntegration({
+      tracePropagationTargets: ['localhost', reactAppWebRoot],
+    }),
+    Sentry.contextLinesIntegration(),
     Sentry.browserProfilingIntegration(),
-    new LocalStorageIntegrations('LocalStorage'),
+    localStorageIntegration('LocalStorage'),
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions
